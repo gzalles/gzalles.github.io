01cfbebfdef7faf7b9e61a7e11b66e3b4bf7bda5
Gabriel Zalles <gzallesb@gmail.com>
2017-06-07 22:26:09 -0400

    contact form via php added

--- php/contact.php
+++ php/contact.php
@@ -1,9 +1,9 @@
-<?php $name = $_POST['Name'];
-$email = $_POST['Email'];
-$subject = $_POST['Subject'];
-$comment = $_COMMENT['Comment'];
-$formcontent="From: $name \n Message: $comment";
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
 $recipient = "gzallesb@gmail.com";
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
